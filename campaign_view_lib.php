cd9d1026b3050a2c3a95d6c44307510566a94fb6
fpassow <fpassow@yahoo.com>
2017-01-09 18:22:41 -0500

    Display array fields as lists

--- campaign_view_lib.php
+++ campaign_view_lib.php
@@ -1,6 +1,19 @@
 <?php
 
 function displayArrayField($campaign, $name) {
+    echo "<ul>\r\n";
+    $i = 0;
+    foreach ($campaign->$name as $x) {
+        echo '<li>'.$x;
+        echo '(<a href="deletearrayitem.php?campaignid='.$campaign->id
+                  .'&fieldname='.$name.'&index='. $i++;
+        echo '">delete</a>)</li>'."\r\n";
+    }
+    echo "</ul>\r\n";
+}
+
+/*
+function displayArrayField($campaign, $name) {
     $i = 0;
     foreach ($campaign->$name as $x) {
         echo '<div class="array_item">'.$x;
@@ -9,3 +22,4 @@ function displayArrayField($campaign, $name) {
         echo '">delete</a>)</div>'."\r\n";
     }
 } 
+*/
