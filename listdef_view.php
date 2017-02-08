76be4cc77938c5a56a05d6432f77ddfdafbd74a0
fpassow <fpassow@yahoo.com>
2017-02-08 09:25:36 -0500

    Campaign view rewritten to use JSON def.

--- listdef_view.php
+++ listdef_view.php
@@ -5,10 +5,9 @@ require('top.php');
 
 <div class="optionsbox">
 <?php
-    displayDefListAsTable('drivers', $lists, 3, '_self');
+    displayDefListAsTable($field, 3, '_self');
 ?>
 </div>
 
 <h2><?= $term ?></h2>
-<h2>How it works:</h2>
 <p><?= $def ?>
