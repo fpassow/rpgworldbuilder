b02023ba294ee03b2b23b5746047bd3ea06fc82e
fpassow <fpassow@yahoo.com>
2017-02-10 09:27:27 -0500

    Static campaign view converted to use the JSON def.

--- campaign_static_view.php
+++ campaign_static_view.php
@@ -1,76 +1,21 @@
 <?php
 $pagetitle = $campaign->title;
 require('top.php');
-?>
 
-<h2><?= $campaign->title ?></h2>
-<div>By <?= $campaign->username ?></div>
+echo '<div>By '.$campaign->username."</div>\r\n";
 
-<p><?= $campaign->seed_text ?></p>
-
-<h2>Campaign Drivers</h2>
-<?php
-    foreach ($campaign->driver as $d) {
-        echo '<div class="array_item">'.$d.'</div>'."\r\n";
-    }
-?>
-
-<h2>Places</h2>
-<?php
-    foreach ($campaign->place as $p) {
-        echo '<div class="array_item">'.$p.'</div>'."\r\n";
-    }
-?>
-
-<h2>PCs are....</h2>
-<p><?= $campaign->pcs_are ?></p>
-
-<h2>People, Groups, and Things</h2>
-<?php
-    foreach ($campaign->person_group_thing as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
-?>
-
-<h2>Adventures</h2>
-<?php
-    foreach ($campaign->adventure as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
-?>
-
-<h2>Classes</h2>
-<?php
-    foreach ($campaign->character_class as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
-?>
-
-<h2>Toys</h2>
-<?php
-    foreach ($campaign->toy as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
-?>
-
-<h2>Treasure</h2>
-<?php
-    foreach ($campaign->treasure as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
-?>
-
-<h2>Players' Intro</h2>
-<p><?= $campaign->players_intro ?></p>
-
-<h2>PC Creation Notes</h2>
-<p><?= $campaign->pc_creation_notes ?></p>
-
-<h2>First Adventures</h2>
-<?php
-    foreach ($campaign->first_adventure as $x) {
-        echo '<div class="array_item">'.$x.'</div>'."\r\n";
-    }
+$fields = $model->getDef()->fields;
+foreach ($fields as $field) {
+    echo '<h2>'.$field->label."</h2>\r\n";
+    $name = $field->name;
+    if ($field->isarrayfield) {
+        foreach ($campaign->$name as $x) {
+            echo '<div class="array_item">'.$x.'</div>'."\r\n";
+        }
+    } else {
+        echo '<div>'.$campaign->$name."</div>\r\n";
+    }  
+}
 ?>
 
 </div> <!-- End #main -->
--- campaign_view.php
+++ campaign_view.php
@@ -14,6 +14,7 @@ if (!isset($is_new_campaign) or !$is_new_campaign) {
 <form action="updatecampaign.php" method="POST">
 <input type="hidden" name="id" value="<?= $campaign->id ?>">
 <input type="hidden" name="focus_here" id="focus_here" value="<?= $focus_here ?>">
+<input type="submit" value="SAVE" class="submit_button">
 
 <?php
 foreach ($model->def->fields as $field) {
@@ -32,87 +33,9 @@ foreach ($model->def->fields as $field) {
     }
 }
 
-
-####################################
-/*
-
-<h2>Campaign Drivers</h2>
-<div class="optionsbox">
-<?php
-    displayDefListAsTable('drivers', $lists, 3, 'deftab');
-?>
-</div>
-<?php
-    displayArrayField($campaign, 'driver');
 ?>
 
-<h2>Places</h2>
-<?php
-    displayArrayField($campaign, 'place');
-?>
-
-<h2>PCs are....</h2>
-<textarea name="pcs_are" id="pcs_are" rows="5" cols="80"><?= $campaign->pcs_are ?></textarea>
-
-<h2>People, Groups, and Things in the World</h2>
-<?php
-   # It's a lot to read. And I think it dictracts more than it helps
-   # displayListAsTable('whos', $lists, 4);
-   
-    displayArrayField($campaign, 'person_group_thing');
-?>
-
-<h2>Adventures (which kinds will work?)</h2>
-<div class="optionsbox">
-<?php
-    displayListAsTable('adventures', $lists, 4);
-?>
-</div>
-<?php
-    displayArrayField($campaign, 'adventure');
-?>
-
-<h2>Classes (fun to be...)</h2>
-<?php
-    displayArrayField($campaign, 'character_class');
-?>
-
-<h2>Toys (fun to use)</h2>
-<div class="optionsbox">
-<?php
-    displayListAsTable('toys', $lists, 4);
-?>
-</div>
-<?php
-    displayArrayField($campaign, 'toy');
-?>
-
-<h2>Treasure (fun payoffs from adventures)</h2>
-<div class="optionsbox">
-<?php
-    displayListAsTable('treasure', $lists, 4);
-?>
-</div>
-<?php
-    displayArrayField($campaign, 'treasure');
-?>
-
-<h2>Players' Intro (what you will tell them)</h2>
-<textarea name="players_intro" id="players_intro" rows="5" cols="80"><?= $campaign->players_intro ?></textarea>
-
-<h2>PC Creation Notes (how to start talking about creating fun characters)</h2>
-<textarea name="pc_creation_notes" id="pc_creation_notes" rows="5" cols="80"><?= $campaign->pc_creation_notes ?></textarea>
-
-<h2>First five adventures, in order</h2>
-<?php
-    displayArrayField($campaign, 'first_adventure');
-?>
-
-*/
-####################################################################
-?>
-
-<br><br><input type="submit" value="SAVE">
+<br><br><input type="submit" value="SAVE"  class="submit_button">
 
 </form>
 
